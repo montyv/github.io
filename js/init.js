125c6343cb078dff8602d4b89159344bcf0ab3c1
monty <velimir.vesselinov@gmail.com>
2021-06-22 22:59:44 -0600

    major

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
+	$('.sidenav').sidenav();
 	$('.materialboxed').materialbox();
 	$('.parallax').parallax();
-	$('.button-collapse').sideNav();
+	$('.collapsible').collapsible();
 });
